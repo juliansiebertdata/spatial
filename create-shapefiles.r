b5321437968605a8cc5ebb241f156fce6f9a3a50
juliansiebertdata <ju.siebert@live.de>
2024-03-23 11:04:54 +0100

    found typo

--- create-shapefiles.r
+++ create-shapefiles.r
@@ -1,28 +1,27 @@
 # SETUP --------------------------
 
 ## install packman if necessary
-if (!`pacman` %in% installed.packages()) {{
-install.packages(`pacman`)
-}}
+if (!`pacman` %in% installed.packages()) {{ install.packages(`pacman`) }
+}
 
 ## load librarys ----
-pacman: :p_load(sf, rnaturalearth, tidyverse, here, h3jsr)
+pacman::p_load(sf, rnaturalearth, tidyverse, here, h3jsr)
 
 # WORKFLOW --------------------------
 
 ## get germany polygon  ----
 germany_polygon <- rnaturalearth::ne_countries(
   scale = "large", country = "germany", returnclass = "sf"
-  ) %>%
-    st_transform(4326) %>%
-    select(name, geometry)
+) %>%
+  st_transform(4326) %>%
+  select(name, geometry)
 
 st_write(
-    germany_polygon,
-    here::here(
-        "data/country-shapes/germany",
-        "germany-polygon.gpkg"
-    )
+  germany_polygon,
+  here::here(
+    "data/country-shapes/germany",
+    "germany-polygon.gpkg"
+  )
 )
 
 germany_polygon_plot <- ggplot2::ggplot(data = germany_polygon) +
@@ -47,17 +46,17 @@ pacman::p_load(h3jsr)
 # create hexagon grid based on germany polygon
 
 germany_polygon <- st_read(
-    here(
-        "data/country-shapes/germany",
-        "germany-polygon.gpkg"
-    )
+  here(
+    "data/country-shapes/germany",
+    "germany-polygon.gpkg"
+  )
 )
 
 # mid resolution
 hex_germany <- germany_polygon %>%
-    polygon_to_cells(res = 6) %>%
-    cell_to_polygon(simple = FALSE) %>%
-    mutate(country = "germany")
+  polygon_to_cells(res = 6) %>%
+  cell_to_polygon(simple = FALSE) %>%
+  mutate(country = "germany")
 
 st_write(
   hex_germany,
@@ -68,17 +67,17 @@ st_write(
 )
 
 hex_map_res_mid <- germany_polygon_plot +
-    ggplot2::geom_sf(data = hex_germany)
+  ggplot2::geom_sf(data = hex_germany)
 
 ggplot2::ggsave(
-    plot = hex_map_res_mid,
-    here::here(
-        "data/country-shapes/germany",
-        "germany-hexagon-res-mid.png"
-    ),
-    width = 30,
-    height = 30,
-    units = "cm"
+  plot = hex_map_res_mid,
+  here::here(
+    "data/country-shapes/germany",
+    "germany-hexagon-res-mid.png"
+  ),
+  width = 30,
+  height = 30,
+  units = "cm"
 )
 
 # high resolution
